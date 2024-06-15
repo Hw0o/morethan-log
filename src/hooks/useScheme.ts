66ab5f205477e8aa1651e8a79d9bd946d612c18e
Addict <dahyeonyun87@gmail.com>
2024-06-15 21:27:08 +0900

    Update useScheme.ts

--- src/hooks/useScheme.ts
+++ src/hooks/useScheme.ts
@@ -9,25 +9,25 @@ type SetScheme = (scheme: Scheme) => void
 const useScheme = (): [Scheme, SetScheme] => {
   const queryClient = useQueryClient()
 
+  // 처음 로드할 때 "light"로 초기화
   const { data } = useQuery({
     queryKey: queryKey.scheme(),
-    enabled: false,
-    initialData: "light",
+    initialData: "light",  // 초기 데이터를 "light"로 설정
   })
 
-  const scheme = data === "light" ? "light" : "dark"
+  // data가 없으면 "light"로 설정
+  const scheme = data === "dark" ? "dark" : "light"
 
   const setScheme = (scheme: "light" | "dark") => {
     setCookie("scheme", scheme)
-
     queryClient.setQueryData(queryKey.scheme(), scheme)
   }
 
   useEffect(() => {
     if (!window) return
 
-    const scheme = getCookie("scheme")
-    setScheme(scheme === "light" ? "light" : "dark")
+    // 처음 로드할 때는 항상 "light"로 설정
+    setScheme("light")
   }, [])
 
   return [scheme, setScheme]
